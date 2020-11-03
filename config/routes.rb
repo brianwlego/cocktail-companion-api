65942751bc917ec7f2af2a1aa0d88316e1777e91
brianwlego <legobw@gmail.com>
2020-11-03 00:02:00 -0500

    Adjusted routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resources :cocktails
-  resources :ingredients
-  resources :measurements
+  resources :cocktails, only: [:index, :show, :create, :update]
+  resources :ingredients, only: [:index, :show]
+  resources :measurements, only: [:create]
 end
